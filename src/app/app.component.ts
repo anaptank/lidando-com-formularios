d41ecd2915e11cabe40be7e9092c423ec88fad48
Ana Paula Pianezer Tank <ana.tank@workdb.com.br>
2021-02-02 21:59:26 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import {
+  Component,
+  ViewChild
+} from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f', { static: false }) signupForm: NgForm;
+  defaultOption = 'Advanced';
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  
+  onSubmit() {
+    console.log(this.signupForm.value)
+  }
+}
